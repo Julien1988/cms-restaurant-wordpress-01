213b2eb851fd05a9bd924ad00fffdefe5473a8c8
Julien <julienbroens@gmail.com>
2020-03-25 15:15:41 +0100

    ajour de la modification de l'icone dans le burger menu en fonction du click

--- app.js
+++ app.js
@@ -2,33 +2,41 @@
 
 (() => {
   const arrayElement = [
-    "<a href='#'><i class='fas fa-home'></i></a>",
-    "<a href='#'>Ours restaurants</a>",
-    "<a href='#'>Menu</a>",
-    "<a href='#'>Recipes</a>",
-    "<a href='#'>Reservations</a>",
-    "<a href='#'>Order</a>",
-    "<a href='#'><i class='fas fa-shopping-cart'></i></a>"
+    "<a class='header__burger-menu__list__item__link' href='#'><i class='fas fa-home'></i></a>",
+    "<a class='header__burger-menu__list__item__link' href='#'>Ours restaurants</a>",
+    "<a class='header__burger-menu__list__item__link' href='#'>Menu</a>",
+    "<a class='header__burger-menu__list__item__link' href='#'>Recipes</a>",
+    "<a class='header__burger-menu__list__item__link' href='#'>Reservations</a>",
+    "<a class='header__burger-menu__list__item__link primary-button' href='#'>Order</a>",
+    "<a class='header__burger-menu__list__item__link' href='#'><i class='fas fa-shopping-cart'></i></a>"
   ];
 
   let getElement = document.querySelector(".header");
   let clickCount = 0;
-
+  let getBurger = document.querySelector(".header__nav__burger-menu__icone");
   document
     .querySelector(".header__nav__burger-menu")
     .addEventListener("click", () => {
       if (clickCount === 0) {
         clickCount++;
         let ul = getElement.appendChild(document.createElement("ul"));
+        ul.className = "header__burger-menu__liste";
+        getBurger.className = "header__nav__burger-menu__icone fas fa-times";
         for (let i = 0; i < arrayElement.length; i++) {
           let li = ul.appendChild(document.createElement("li"));
+          li.className = "header__burger-menu__liste__item";
           li.innerHTML = arrayElement[i];
         }
         console.log(clickCount);
         return clickCount;
       } else {
         clickCount--;
-        getElement.removeChild(getElement.lastChild);
+        let removeElement = document.querySelector(
+          ".header__burger-menu__liste"
+        );
+        getBurger.className = "header__nav__burger-menu__icone fas fa-bars";
+        removeElement.remove(removeElement);
+        //getElement.removeChild(getElement.lastChild);
         return clickCount;
       }
     });
