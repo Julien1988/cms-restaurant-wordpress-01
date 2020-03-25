e1d7859dfef15c6a6dfee2d886cd0affff393058
Julien <julienbroens@gmail.com>
2020-03-25 12:42:44 +0100

    création de la nav-bar ok

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+// Gesttion de la nav bar pour la vue mobile
+
 (() => {
-  let arrayElement = [
+  const arrayElement = [
     "<a href='#'><i class='fas fa-home'></i></a>",
     "<a href='#'>Ours restaurants</a>",
     "<a href='#'>Menu</a>",
@@ -10,14 +12,24 @@
   ];
 
   let getElement = document.querySelector(".header");
+  let clickCount = 0;
 
   document
     .querySelector(".header__nav__burger-menu")
     .addEventListener("click", () => {
-      let ul = getElement.appendChild(document.createElement("ul"));
-      for (let i = 0; i < arrayElement.length; i++) {
-        let li = ul.appendChild(document.createElement("li"));
-        li.innerHTML = arrayElement[i];
+      if (clickCount === 0) {
+        clickCount++;
+        let ul = getElement.appendChild(document.createElement("ul"));
+        for (let i = 0; i < arrayElement.length; i++) {
+          let li = ul.appendChild(document.createElement("li"));
+          li.innerHTML = arrayElement[i];
+        }
+        console.log(clickCount);
+        return clickCount;
+      } else {
+        clickCount--;
+        getElement.removeChild(getElement.lastChild);
+        return clickCount;
       }
     });
 })();
